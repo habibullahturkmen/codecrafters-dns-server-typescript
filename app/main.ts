f8eee9e93ae754652e8432123eb822b79d4444ac
Habibullah Turkmen <habibullah_turkmen@yahoo.com>
2026-01-07 14:33:04 -0500

    Setup UDP server

--- app/main.ts
+++ app/main.ts
@@ -1,19 +1,16 @@
 import * as dgram from "dgram";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+console.log("running");
 
-// TODO: Uncomment the code below to pass the first stage
-//
-// const udpSocket: dgram.Socket = dgram.createSocket("udp4");
-// udpSocket.bind(2053, "127.0.0.1");
-//
-// udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
-//     try {
-//         console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
-//         const response = Buffer.from("");
-//         udpSocket.send(response, remoteAddr.port, remoteAddr.address);
-//     } catch (e) {
-//         console.log(`Error sending data: ${e}`);
-//     }
-// });
+const udpSocket: dgram.Socket = dgram.createSocket("udp4");
+udpSocket.bind(2053, "127.0.0.1");
+
+udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
+    try {
+        console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
+        const response = new Uint8Array(Buffer.from(""));
+        udpSocket.send(response, remoteAddr.port, remoteAddr.address);
+    } catch (e) {
+        console.log(`Error sending data: ${e}`);
+    }
+});
